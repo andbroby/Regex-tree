5787387ab2d2427192bef1d3e6f9417cd4bc82a8
Andreas Broby <and.broby@gmail.com>
2015-03-01 19:30:26 +0100

    added a re class so we can compile regexes

---
+++ src/com/andbroby/re.java
@@ -0,0 +1,29 @@
+package com.andbroby;
+
+/**
+ * Created by broby on 01/03/15.
+ */
+public class re {
+    RegexNode regexp;
+
+    public re(RegexNode re) {
+        this.regexp = re;
+    }
+
+    public boolean match(String string) {
+        boolean result;
+
+        if (string.isEmpty()) {
+            return regexp.canMatchEmptyString();
+        }
+
+        result = regexp.shift(string.charAt(0), true);
+        for (int i = 1; i < string.length(); i++) {
+            result = regexp.shift(string.charAt(i), false);
+        }
+
+        regexp.reset();
+
+        return result;
+    }
+}
